24188bb77d78fb9d52992a01e922b9f724510fcd
Jackeagle <jackeagle102@gmail.com>
2020-10-28 00:33:33 -0400

    Blissify Rog 2
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- bliss_I001D.mk
+++ bliss_I001D.mk
@@ -5,14 +5,16 @@
 #
 
 $(call inherit-product, device/asus/I001D/device.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := I001D
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := ASUS_I001_1
-PRODUCT_NAME := lineage_I001D
+PRODUCT_NAME := bliss_I001D
+
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
