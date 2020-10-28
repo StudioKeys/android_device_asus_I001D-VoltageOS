72ed141ec8d42a595a836e5d484248ab8c0d3a52
Jackeagle <jackeagle102@gmail.com>
2020-10-28 00:39:53 -0400

    I001D: Add Recovery AVB Flags
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,7 +136,13 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+ifeq ($(BOARD_AVB_ENABLE), true)
+   BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+   BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+   BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+   BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+   BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+endif
 
 # Inherit from the proprietary version
 -include vendor/asus/I001D/BoardConfigVendor.mk
