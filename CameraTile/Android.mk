LineageOS/android_device_asus_I01WD

I01WD: Add a tile to trigger camera rotation control
Change-Id: I4cca7b45197ce7ec1743589bcae3e1d04e37c68b

CameraTile/Android.mk
@@ -1,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src)
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res
+
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay
+
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_SDK_VERSION := current
+
+LOCAL_PACKAGE_NAME := CustomTiles
+
+include $(BUILD_PACKAGE)

StudioKeys/android_device_asus_I001D-VoltageOS

CameraTile/Android.mk
@@ -1,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src)
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res
+
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay
+
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_SDK_VERSION := current
+
+LOCAL_PACKAGE_NAME := CameraTile
+
+include $(BUILD_PACKAGE)